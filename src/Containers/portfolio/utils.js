2c2363ef25876e397956d54cf7dfed12b9ef2c16
meet savaliya <msavaliya023@gmail.com>
2023-02-08 19:52:07 +0530

    third commit

--- src/Containers/portfolio/utils.js
+++ src/Containers/portfolio/utils.js
@@ -4,19 +4,19 @@ import imageThree from '../../images/image3.png'
 
 export const portfolioData = [
     {
-        Image: imageOne,
+        pngg: imageOne,
         projectName: "ChatApp",
         projectTitle: "Chateo is a messaging app for Android and Ios that uses React native and Firebase. Its user interface is based on Material Components, providing a modern and user-friendly design.",
         projectLink: "https://github.com/meet003/ChatApp-using-react-native",
     },
     {
-        Image: imageTwo,
+        pngg: imageTwo,
         projectName: "InvoiceBill App",
         projectTitle: "It is Basically an invoice bill generator mobile app designed for local shops where shop owners can generate bill on the spot and send to customer in Pdf format",
         projectLink: "https://github.com/meet003/Invoice-Bill-Maker-using-React-Native-",
     },
     {
-        Image: imageThree,
+        pngg: imageThree,
         projectName: "Meeting Web_app",
         projectTitle: "Developed and Designed a real-time application for multiple users to do end to end encrypted video call and chat.",
         projectLink: "https://github.com/meet003/Real-time-end-to-end-encrypted-video-call-and-chat-using-Nodejs",
